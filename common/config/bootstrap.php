dc30de3a7df1d32a06d6c23d4d141bdd1138f8ae
a.kuznecov <a.kuznecov@uremont.com>
2018-03-13 13:59:35 +0300

    second commit + add front (react+angular+socket), add api

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,3 +3,9 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
+Yii::setAlias('@frontend_react', dirname(dirname(__DIR__)) . '/frontend_react');
+Yii::setAlias('@frontend_angular', dirname(dirname(__DIR__)) . '/frontend_angular');
+Yii::setAlias('@frontend_socket', dirname(dirname(__DIR__)) . '/frontend_socket');
+
+
---
+++ common/config/sample.main-local.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=database',
+            'username' => 'localuser',
+            'password' => '',
+            'charset' => 'utf8',
+            'tablePrefix'         => '',
+            'enableSchemaCache'   => true,
+            'schemaCacheDuration' => 0,
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
